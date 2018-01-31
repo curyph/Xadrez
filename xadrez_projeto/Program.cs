e584d9be67d1483aeca8d2997196919df24ed2bf
Pedro Cury <curyph@gmail.com>
2018-01-31 13:52:08 -0200

    Tabuleiro tela

--- xadrez_projeto/Program.cs
+++ xadrez_projeto/Program.cs
@@ -11,9 +11,11 @@ namespace xadrez_projeto
     {
         static void Main(string[] args)
         {
-            Posicao P = new Posicao(3, 4);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            
+            Tela.imprimirTabuleiro(tab);
 
-            Console.WriteLine("Posição : " + P);
+            
             Console.ReadLine();
         }
     }
